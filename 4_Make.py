84bb8de68e632e8c552055fe3c61821466cdab2c
FourX11gd <levengoodjohn@gmail.com>
2024-09-27 08:23:15 +0000

    I hope this works

--- 4_Make.py
+++ 4_Make.py
@@ -22,8 +22,69 @@ Key design considerations:
 
 Test data:
   I accidentally deleted the first program, but this one will have the same testing, multiple numbers to check the math
-  Numbers to check menuing, including indicating a incorrect number and asking again
-
+  Numbers to check menuing, including indicating a incorrect number and asking again.  While testing I did some
+  fomating to make easier to read the actual program
 '''
+#setting constants and defining functions
+operation = ''
+exit = '1'
+menu = '0'
 
-#setting 
+def add(a, b):
+    global operation
+    operation = 'added'
+
+    return (a + b)
+
+def subtract(c, d):
+    global operation
+    operation = 'subtracted'
+
+    return (c - d)
+
+def multiply(e, f):
+    global operation
+    operation = 'multiplied'
+
+    return(e * f)
+
+def divide(g, h):
+    global operation
+    operation = 'divided'
+
+    return(g / h)
+
+#begin main part of program, ask for numbers and menu choices
+
+while exit != '0':
+    print('\nEnter your choice of numbers. Remember position matters for subtraction and division')
+    num1 = int(input('Enter first number: '))
+    num2 = int(input('Enter second number: '))
+    print('\nEnter number to select mathmatical function\n 1 for addition \n 2 for subtraction \n 3 for multiplication \n 4 for division')
+
+    while menu != '1' or '2' or '3' or '4':
+        menu = input('\nSelect mathmatical function: ')
+        if menu == '1':
+            math = add(num1, num2)
+            break
+
+        elif menu == '2':
+            math = subtract(num1, num2)
+            break
+
+        elif menu == '3':
+            math = multiply(num1, num2)
+            break
+
+        elif menu == '4':
+            math = divide(num1, num2)
+            #Couldn't remember a way to keep decimal places short, when online to find the round funciton
+            math = round(math, 2)
+            break
+
+        else:
+            print('\nYou have entered a incorrect menu option, please try again')
+    
+    print(f'\nYour first number {num1} was {operation} by your second number {num2} for a total of {math}!')
+
+    exit = input('\nEnter 0 to exit, anything else to try again: ')
