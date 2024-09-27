3ff8ca2280ba73a2d6b756f6fbf00a8ce66c08fd
FourX11gd <levengoodjohn@gmail.com>
2024-09-27 00:40:02 -0600

    Update 4_Make.py

--- 4_Make.py
+++ 4_Make.py
@@ -1,9 +1,22 @@
 '''
-Define four subroutines - add, subtract, multiply, divide that add multiply etc two numbers and return the result. Each should have two integer number arguments.
-The user is asked to input two numbers. These numbers will be passed as arguments into one of the subroutines.
-The user is asked to input 1 to add, 2 to subtract etc.
-If they input 1, call the ‘add’ subroutine, input 2 calls the ‘subtract’ subroutine etc
-Output the returned result as part of a sentence.
+File:  cosc-1010-fall-fuctions-FourX11gd
+
+Name:  John Levengood
+
+Requirements:  Define four subroutines - add, subtract, multiply, divide that add multiply etc two numbers and return the result. Each should have two integer number arguments.
+  The user is asked to input two numbers. These numbers will be passed as arguments into one of the subroutines.
+  The user is asked to input 1 to add, 2 to subtract etc.
+  If they input 1, call the ‘add’ subroutine, input 2 calls the ‘subtract’ subroutine etc
+  Output the returned result as part of a sentence.
+
+Variables:
+  Inptus: Menu Inputes and Numerical Inputs
+  Calculated:  Mathematical statements
+  
+Outputs:  Mathemeatcal statements, dynamic sentances
+
+Key calculations:  Mathematical satements, Menu selections
+
 
 
 '''
